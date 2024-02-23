373a16b17c001e3a59b7231d2262455cea885913
Kotgoth <demeneff@gmail.com>
2024-02-23 14:32:23 +0300

    3400

--- components/delumo/output/delumo_output.cpp
+++ components/delumo/output/delumo_output.cpp
@@ -194,7 +194,7 @@ void DelumoOutput::send_byte_(uint8_t byte) {
   //   // wait for HIGH level: // MRF reports READY by high MISO
   // };
   this->delegate_->transfer(byte);
-  ets_delay_us(2600);
+  ets_delay_us(3400);
 }
 
 void DelumoOutput::send_command_(uint16_t command) {
